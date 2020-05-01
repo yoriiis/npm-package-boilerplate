84963c28427068ab0f22face56f70abf4379dcc5
Yoriiis <joris.daniel@gmail.com>
2020-05-01 20:23:47 +0200

    Add Typescript

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @license MIT
+ * @name npmPackageBoilerplate
+ * @version 1.0.0
+ * @author: Yoriiis aka Joris DANIEL <joris.daniel@gmail.com>
+ * @description: Boilerplate to kickstart creating a npm package
+ * {@link https://github.com/yoriiis/npm-package-boilerplate}
+ * @copyright 2020 Joris DANIEL
+ **/
+declare const _default: () => string;
+export = _default;
